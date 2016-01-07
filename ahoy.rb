b042d56ad7e29340fc4f6d571eeca601cef824db
Frank Carey <frank@devinci.io>
2016-01-07 12:56:28 -0500

    Bump to version 1.1.0

--- ahoy.rb
+++ ahoy.rb
@@ -1,9 +1,9 @@
 class Ahoy < Formula
   desc "Ahoy - Create aliases for commands on docker, ssh, and your local machine."
   homepage "https://github.com/devinci-code/ahoy"
-  url "https://github.com/devinci-code/ahoy/releases/download/1.0.0/ahoy-release-1-0-0.tar.gz"
-  version "1.0.0"
-  sha256 "927717589f8e78c706d4e60816f6e49a66dcef925aa73ab91baff7b2c8d3d770"
+  url "https://github.com/devinci-code/ahoy/releases/download/1.1.0/ahoy-darwin-amd64"
+  version "1.1.0"
+  sha256 "8bb1c76e28ca499f2dcadafdec7776ca3bd4e85c45c8d4f9f19220d5d4f233cf"
 
   def install
     system "mv", "ahoy-darwin-amd64", "ahoy"
