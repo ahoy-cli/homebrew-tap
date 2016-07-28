27fe219c9495663096f350d3bdc84302de8d5bca
Frank Carey <frank@devinci.io>
2016-07-27 20:29:29 -0400

    Add an option to build ahoy from master using the --HEAD flag.

--- ahoy.rb
+++ ahoy.rb
@@ -1,12 +1,29 @@
+require "language/go"
+
 class Ahoy < Formula
   desc "Ahoy - Create aliases for commands on docker, ssh, and your local machine."
   homepage "https://github.com/devinci-code/ahoy"
-  url "https://github.com/devinci-code/ahoy/releases/download/1.1.0/ahoy-darwin-amd64"
+  url "https://github.com/DevinciHQ/ahoy/releases/download/1.1.0/ahoy-darwin-amd64"
   version "1.1.0"
   sha256 "8bb1c76e28ca499f2dcadafdec7776ca3bd4e85c45c8d4f9f19220d5d4f233cf"
 
+  head "https://github.com/DevinciHQ/ahoy.git",
+    :branch => "master"
+
+  depends_on "go" => :build
+
   def install
-    system "mv", "ahoy-darwin-amd64", "ahoy"
-    bin.install "ahoy"
+    # Build from the latest verison of ahoy.
+    if build.head?
+      ENV["GOPATH"] = buildpath
+      path = buildpath/"src/github.com/DevinciHQ/ahoy"
+      path.install Dir["*"]
+      Language::Go.stage_deps resources, buildpath/"src"
+      system "go", "build", "-o", "bin/ahoy", "github.com/DevinciHQ/ahoy"
+      bin.install "bin/ahoy"
+    else
+      system "mv", "ahoy-darwin-amd64", "ahoy"
+      bin.install "ahoy"
+    end
   end
 end
