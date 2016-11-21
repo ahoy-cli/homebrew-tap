44b5fa1cdab5518a7a4197475a5fa111c777a811
Frank Carey <frank@devinci.io>
2016-11-21 12:30:51 -0500

    Update ahoy paths after moving ahoy to the new ahoy-cli github organization.

--- ahoy.rb
+++ ahoy.rb
@@ -2,12 +2,12 @@ require "language/go"
 
 class Ahoy < Formula
   desc "Ahoy - Create aliases for commands on docker, ssh, and your local machine."
-  homepage "https://github.com/devinci-code/ahoy"
-  url "https://github.com/DevinciHQ/ahoy/releases/download/1.1.0/ahoy-darwin-amd64"
+  homepage "http://www.ahoycli.com/"
+  url "https://github.com/ahoy-cli/ahoy/releases/download/1.1.0/ahoy-darwin-amd64"
   version "1.1.0"
   sha256 "8bb1c76e28ca499f2dcadafdec7776ca3bd4e85c45c8d4f9f19220d5d4f233cf"
 
-  head "https://github.com/DevinciHQ/ahoy.git",
+  head "https://github.com/ahoy-cli/ahoy.git",
     :branch => "master"
 
   depends_on "go" => :build
@@ -16,10 +16,10 @@ class Ahoy < Formula
     # Build from the latest verison of ahoy.
     if build.head?
       ENV["GOPATH"] = buildpath
-      path = buildpath/"src/github.com/DevinciHQ/ahoy"
+      path = buildpath/"src/github.com/ahoy-cli/ahoy"
       path.install Dir["*"]
       Language::Go.stage_deps resources, buildpath/"src"
-      system "go", "build", "-o", "bin/ahoy", "github.com/DevinciHQ/ahoy"
+      system "go", "build", "-o", "bin/ahoy", "github.com/ahoy-cli/ahoy"
       bin.install "bin/ahoy"
     else
       system "mv", "ahoy-darwin-amd64", "ahoy"
