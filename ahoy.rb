eefc045894f19504da7e4c778ec69edc884de99c
Frank Carey <fcarey@gmail.com>
2017-06-13 12:36:02 -0400

    Bump version to 2.0.0.

--- ahoy.rb
+++ ahoy.rb
@@ -1,9 +1,9 @@
 class Ahoy < Formula
   desc "Creates self documenting cli programs from commands in yaml files."
   homepage "http://www.ahoycli.com/"
-  url "https://github.com/ahoy-cli/ahoy/releases/download/2.0.0-beta1/ahoy-bin-darwin-amd64"
-  version "2.0.0-beta1"
-  sha256 "9c06abef1b819f1d67c63d210cb8d232348e13151269d3bffd306d5a26eee148"
+  url "https://github.com/ahoy-cli/ahoy/releases/download/2.0.0/ahoy-bin-darwin-amd64"
+  version "2.0.0"
+  sha256 "628d239626620ddf0199670a4e91366fa1422ed0cb7c26d2cd0537a8f90937ed"
 
   head "https://github.com/ahoy-cli/ahoy.git",
     :branch => "master"
@@ -20,7 +20,7 @@ class Ahoy < Formula
 
     See other changes at:
 
-    https://github.com/ahoy-cli/ahoy#version-2
+    https://github.com/ahoy-cli/ahoy
 
     EOS
   end
